b6af16089837173de83ce832b61476620bf1b8bb
a-kenji <aks.kenji@protonmail.com>
2022-03-07 14:44:13 +0100

    add: nci
    errors currently because it can't find `cargo`

--- nix/default.nix
+++ nix/default.nix
@@ -9,21 +9,7 @@
 
 nixCargoIntegration.lib.makeOutputs {
     root = ../.;
-    #buildPlatform = "crate2nix";
     defaultOutputs = { app = "zellij"; package = "zellij"; };
-     #overrides = {
-        #crateOverrides = common: prev:
-            #let
-                #pkgs = common.pkgs;
-                #lib  = common.lib;
-            #in
-            #{
-            #zellij = _prev: {
-            #nativeBuildInputs = [ pkgs.cargo ];
-            #BuildInputs = [ pkgs.cargo ];
-            #};
-        #};
-        #};
     }
 
 #}
